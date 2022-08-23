632fd17cd6303f68c977a32873370b5f7a195124
Josh <joshua.sanders@periapsistech.com>
2022-08-23 11:04:53 -0400

    final cleanup

--- MiniTomatoMaui/Services/TextAnalyzerService.cs
+++ MiniTomatoMaui/Services/TextAnalyzerService.cs
@@ -37,6 +37,11 @@ public class TextAnalyzerService
     {
         try
         {
+            // Http Client Instantiation
+            var httpClient = new HttpClient();
+            httpClient.BaseAddress = new Uri("https://moodanalyzer.cognitiveservices.azure.com/text/analytics/v3.0/sentiment");
+            httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
             // Text Request
             TextAnalyzerRequest textRequest = new()
             {
@@ -48,15 +53,11 @@ public class TextAnalyzerService
                 }
             };
 
-            var httpClient = new HttpClient();
-            httpClient.BaseAddress = new Uri("https://moodanalyzer.cognitiveservices.azure.com/text/analytics/v3.0/sentiment");
-            httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-
             // Serialize Data
             var myContent = JsonConvert.SerializeObject(textRequest);
             StringContent data = new StringContent(myContent, Encoding.UTF8, "application/json");
 
-            // Request
+            // Http Request
             var request = new HttpRequestMessage
             {
                 Method = HttpMethod.Post,
@@ -70,8 +71,9 @@ public class TextAnalyzerService
 
             var response = await httpClient.SendAsync(request);
             var item = response.Content.ReadAsStringAsync();
-            var sentiments = Newtonsoft.Json.JsonConvert.DeserializeObject<TextAnalyzerResponse>(item.Result);
+            var sentiments = JsonConvert.DeserializeObject<TextAnalyzerResponse>(item.Result);
 
+            // Retrieving only 1 document for now
             var sentiment = sentiments.documents.FirstOrDefault();
 
             return sentiment;
